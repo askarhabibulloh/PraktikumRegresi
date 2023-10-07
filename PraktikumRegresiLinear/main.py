476d6fc4739239aca4b8116e7ae83241cb0e1a57
askarhabibulloh <mhmmdaskrhbblh@gmail.com>
2023-10-07 18:45:31 +0700

    7 Oktober 2023

--- PraktikumRegresiLinear/main.py
+++ PraktikumRegresiLinear/main.py
@@ -1,4 +1,13 @@
-x = [10, 28, 29, 35, 48, 55, 71, 73, 80, 88, 91, 111, 131, 144, 160, 170]
+x = [10, 28, 29, 35, 48, 55, 71, 73, 80, 88, 91, 111, 131, 144, 160]
 y = [29, 47, 55, 65, 79, 82, 92, 95, 100, 102, 110, 124, 127, 130, 152]
 xy = [x * y for x, y in zip(x, y)]
-xkuadrat = [x * y for x, y in zip(x, y)]
+kuadx = [x * y for x, y in zip(x, x)]
+sigmaxy = sum(xy)
+sigmax = sum(x)
+sigmay = sum(y)
+sigmakuadx = sum(kuadx)
+a = (sigmay*sigmakuadx-sigmax*sigmaxy)/(15*sigmakuadx-sigmax*sigmax)
+b = (15*sigmaxy-sigmax*sigmay)/(15*sigmakuadx-sigmax*sigmax)
+angkaselanjutnya = int(input("Masukkan biaya promosi : "))
+perkiraan = a+b*angkaselanjutnya
+print("perkiraan penjualan rumah adalah : ", perkiraan)
